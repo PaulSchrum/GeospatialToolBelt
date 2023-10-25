1ba7e72072242dd4eae246098c652bbf3acf33fd
Paul Schrum <paul.schrum@gmail.com>
2023-10-25 13:08:22 -0400

    Initial Commit.

---
+++ GeoTBelt/Band.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTBelt
+{
+    public class Band
+    {
+        public dynamic?[,] CellArray { get; protected set; }
+        public Raster MyParent { get; protected set; }
+
+        public Band(Raster parent)
+        { 
+            MyParent = parent;
+        }
+    }
+}
