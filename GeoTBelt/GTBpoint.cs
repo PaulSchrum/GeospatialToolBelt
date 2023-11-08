735fa008f8f6853901fe17bdae4d55326e0989af
Paul Schrum <paul.schrum@gmail.com>
2023-11-07 21:23:15 -0500

    Added new point class.

---
+++ GeoTBelt/GTBpoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTBelt
+{
+    public class GTBpoint
+    {
+        public double X { get; protected set; }
+        public double Y { get; protected set; }
+        public double Z { get; protected set; } = 0;
+        public GTBpoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public GTBpoint(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
