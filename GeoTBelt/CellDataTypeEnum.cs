50b6dd4651c03f198031bcb9036d6a10faeee402
Paul Schrum <paul.schrum@gmail.com>
2023-12-21 15:01:24 -0500

    Initial Commit

---
+++ GeoTBelt/CellDataTypeEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTBelt
+{
+    public enum CellDataTypeEnum
+    {
+        Unknown = 0,
+        Byte = 1,
+        Int16 = 2,
+        UInt16 = 3,
+        Int32 = 4,
+        UInt32 = 5,
+        Float = 5,
+        Double = 6,
+        Complex = 7,
+        Decimal = 8,
+    }
+}
