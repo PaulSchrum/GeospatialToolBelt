0f80ab38c5c346fb3cf930c9d0c7f084c70a723e
Paul Schrum <paul.schrum@gmail.com>
2024-01-01 21:23:36 -0500

    Added test for large a GeoTiff.

--- Test_ToolBelt/RasterTests.cs
+++ Test_ToolBelt/RasterTests.cs
@@ -41,16 +41,6 @@ namespace Test_ToolBelt
 
         }
 
-        private static void initializeSingleBandTiff()
-        {
-            if (geoTiffRaster_singleBand is null)
-            {
-                // geoTiffFileName = "NCSU Biltmore Hall small uncomp.tif";
-                geoTiffFileName_singleBand = "TestData_00765413_uncompressed.tif";
-                geoTiffFileFullPath_singleBand = Path.Combine(currentDirectory, geoTiffFileName_singleBand);
-                geoTiffRaster_singleBand = (GeoTiffRaster)Raster.Load(geoTiffFileFullPath_singleBand);
-            }
-        }
 
         private static string improvedGetCurrentDirectory(string desiredDirectory)
         {
@@ -103,12 +93,23 @@ namespace Test_ToolBelt
         }
         #endregion ASC format tests
 
+        private static void initializeSingleBandTiff()
+        {
+            if (geoTiffRaster_singleBand is null)
+            {
+                // geoTiffFileName = "NCSU Biltmore Hall small uncomp.tif";
+                geoTiffFileName_singleBand = "TestData_00765413_uncompressed.tif";
+                geoTiffFileFullPath_singleBand = Path.Combine(currentDirectory, geoTiffFileName_singleBand);
+                geoTiffRaster_singleBand = (GeoTiffRaster)Raster.Load(geoTiffFileFullPath_singleBand);
+            }
+        }
+
         [TestMethod]
         public void GeoTiff_PopulatesSingleBand()
         {
             initializeSingleBandTiff();
             Assert.IsNotNull(geoTiffRaster_singleBand);
-
+            
             Assert.AreEqual(
                 expected: 2, 
                 actual: geoTiffRaster_singleBand.RowsPerStrip);
@@ -155,6 +156,65 @@ namespace Test_ToolBelt
             catch (Exception ex) { }
             Assert.IsTrue(exceptionThrown);
         }
+
+
+        private static GeoTiffRaster geoTiffRaster_5BandSmall;
+        private static string geoTiffFileName_5BandSmall;
+        private static string geoTiffFileFullPath_5BandSmall;
+
+        private static void initialize5BandSmallTiff()
+        {
+            if (geoTiffRaster_5BandSmall is null)
+            {
+                geoTiffFileName_5BandSmall = "NCSU Biltmore Hall small uncomp.tif";
+                geoTiffFileFullPath_5BandSmall = Path.Combine(currentDirectory, geoTiffFileName_5BandSmall);
+                geoTiffRaster_5BandSmall = (GeoTiffRaster)Raster.Load(geoTiffFileFullPath_5BandSmall);
+            }
+        }
+
+        [TestMethod]
+        public void GeoTiff_small_Populates5Band()
+        {
+            initialize5BandSmallTiff();
+            Assert.IsNotNull(geoTiffRaster_5BandSmall);
+
+            Assert.AreEqual(
+                expected: 2,
+                actual: geoTiffRaster_5BandSmall.RowsPerStrip);
+
+            Assert.AreEqual(expected: 5,
+                actual: geoTiffRaster_5BandSmall.bands.Count);
+        }
+
+
+        private static GeoTiffRaster geoTiffRaster_5BandLarge;
+        private static string geoTiffFileName_5BandLarge;
+        private static string geoTiffFileFullPath_5BandLarge;
+
+        private static void initialize5BandLargeTiff()
+        {
+            if (geoTiffRaster_5BandLarge is null)
+            {
+                geoTiffFileName_5BandLarge = "NCSU Jordan Hall uncomp.tif";
+                geoTiffFileFullPath_5BandLarge = Path.Combine(currentDirectory, geoTiffFileName_5BandLarge);
+                geoTiffRaster_5BandLarge = (GeoTiffRaster)Raster.Load(geoTiffFileFullPath_5BandLarge);
+            }
+        }
+
+        [TestMethod]
+        public void GeoTiff_large_Populates5Band()
+        {
+            initialize5BandLargeTiff();
+            Assert.IsNotNull(geoTiffRaster_5BandLarge);
+
+            Assert.AreEqual(
+                expected: 2,
+                actual: geoTiffRaster_5BandLarge.RowsPerStrip);
+
+            Assert.AreEqual(expected: 5,
+                actual: geoTiffRaster_5BandLarge.bands.Count);
+        }
+
 
         [ClassCleanup]
         public static void Cleanup()
